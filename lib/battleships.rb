b9d190a4f8b27f33ccb1b0eacf803856a211da84
mala23 <mail@mala23.me>
2014-09-29 14:37:51 +0100

    Added views index.erb and start.erb and starting_a_game.feature

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,10 +1,18 @@
 require 'sinatra/base'
 
 class BattleShips < Sinatra::Base
-  get '/' do
-    'Hello BattleShips!'
-  end
 
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+	set :views, Proc.new {File.join root, '..', '/views'}
+
+	get '/' do
+		erb :index
+	end
+
+	get '/start' do
+		erb :start
+	end
+
+	# start the server if ruby file executed directly
+	run! if app_file == $0
+
 end
